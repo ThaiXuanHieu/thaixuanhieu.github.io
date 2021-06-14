7d078395858edd4062f54f3282ccf1f00dc6834c
ThaiXuanHieu <s2freedom6789@gmail.com>
2021-06-14 18:01:05 +0700

    update ui

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
-var btnScroll = document.getElementById("btnScroll");
+var slideIndex = 1;
+showSlides(slideIndex);
 
-window.onscroll = function () {
-  scrollFunction()
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    btnScroll.style.display = "block";
-  } else {
-    btnScroll.style.display = "none";
-  }
-
-
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function scrollToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide--item");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
